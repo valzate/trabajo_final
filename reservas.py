6094a2ddda2f3da37d326542fd744879763ba420
valzate <valzate223@gmail.com>
2025-08-23 12:52:27 -0500

    se agrega función para ingresar datos

--- reservas.py
+++ reservas.py
@@ -1,2 +1,12 @@
 # Matriz doblemente anidada
 reservas = []
+# Función para agresar una nueva reserva
+def ingresar_reserva():
+    cod_est = input("Código del estudiante: ")
+    nom_est = input("Nombre del estudiante: ")
+    cod_lib = input("Código del libro: ")
+    nom_lib = input("Nombre del libro: ")
+
+    reservas.append([cod_est, nom_est, cod_lib, nom_lib])
+    print("✅ Reserva agregada correctamente.\n")
+
