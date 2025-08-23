fb6a101bba2ab51713731a5dd6c154df76c1b6da
valzate <valzate223@gmail.com>
2025-08-23 12:29:38 -0500

    se agrega función para ingresar y guardar los datos

--- reservas.js
+++ reservas.js
@@ -1,3 +1,13 @@
-reservas = [
-  [codigoEstudiante, nombreEstudiante, codigoLibro, nombreLibro],
-]
+const prompt = require("prompt-sync")(); // Para poder ingresar los datos por cosola 
+
+let reservas = [];
+// Ingresar una nueva reserva
+function ingresarReserva() {
+  const codigoEstudiante = prompt("Código del estudiante: ");
+  const nombreEstudiante = prompt("Nombre del estudiante: ");
+  const codigoLibro = prompt("Código del libro: ");
+  const nombreLibro = prompt("Nombre del libro: ");
+
+  reservas.push([codigoEstudiante, nombreEstudiante, codigoLibro, nombreLibro]);
+  console.log("✅ Reserva agregada correctamente.\n");
+}
